9039f77d9f5f7359281a7e4dfe9f8d7e1f1b9a8a
bentubox <b2t848r8o@gmail.com>
2016-01-15 21:36:16 -0600

    Initial commit.

---
+++ ProjectOther/ProjectOther/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectOther
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Main
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Other())
+                game.Run();
+        }
+    }
+#endif
+}
